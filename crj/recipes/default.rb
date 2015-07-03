882595da273e389beeaae83c0d18eff7a5e92bad
Tim <tkeller.online@googlemail.com>
2015-07-03 19:31:57 +0200

    Adjust backup time.

--- crj/recipes/default.rb
+++ crj/recipes/default.rb
@@ -14,7 +14,7 @@ node[:deploy].each do |application, deploy|
   cron_env = {'PATH' => '/usr/local/bin:$PATH'}
   cron 'daily data backup cron' do
     environment cron_env
-    hour 17
+    hour 18
     minute 0
     command "RAILS_ENV=#{deploy[:rails_env]} cd #{deploy[:deploy_to]}/current && bundle exec rake data_backup:backup"
   end
